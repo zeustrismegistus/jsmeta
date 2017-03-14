77b7e1c4766c002e5390cf011a839ec6013aa769
zeustrismegistus <zeustrismegistus@tutanota.com>
2017-03-14 18:58:49 -0400

    first commit

---
+++ jsmeta.js
@@ -0,0 +1,377 @@
+(function () {
+
+	const ArrayUtil = 
+	{
+		areArraysEqual : function(a,b) 
+		{
+			if (a === b) return true;
+			if (a == null || b == null) return false;
+			
+			if(!Array.isArray(a))
+				throw "not array";
+			
+			if(!Array.isArray(b))
+				throw "not array";
+			
+			if (a.length != b.length) return false;
+
+			// If you don't care about the order of the elements inside
+			// the array, you should sort both arrays here.
+
+			for (var i = 0; i < a.length; ++i) {
+				if (a[i] !== b[i]) return false;
+			}
+			
+			return true;
+		},
+		removeSet : function(source, toRemove)
+		{
+			if (source === null) return [];
+			if (toRemove === null) return source.slice();
+		
+			var rv = source.filter(function(item)
+			{
+				return toRemove.indexOf(item) < 0;
+			});
+			
+			return rv;
+		}
+	};
+	
+	(function(){
+		//lock it down
+		Object.freeze(ArrayUtil);
+	})();
+
+	const JSMeta = 
+	{
+		/*primative is tests------------------------------------------*/
+		isNull : function(val)
+		{
+			"use strict";
+			return val === null;
+		},
+		isUndefined : function(val)
+		{
+			"use strict";
+			return val === undefined;
+		},
+		isNullOrUndefined : function(val)
+		{
+			"use strict";
+			return JSMeta.isUndefined(val)  || JSMeta.isNull(val);
+		},
+		isEmpty : function(val)
+		{
+			for(var prop in val) {
+				if(val.hasOwnProperty(prop))
+					return false;
+			}
+
+			return JSON.stringify(val) === JSON.stringify({});
+		},
+		/*is something a javascript primitive type or an array*/
+		isPrimitive : function(val)
+		{
+			"use strict";
+				
+			if(JSMeta.isNullOrUndefined(val))
+				return true;
+			
+			var valType = typeof val;
+
+			var rv = false;
+			
+			if(valType === "boolean")
+			{
+				rv = true;
+			}
+			else if (valType === "number")
+			{
+				rv = true;
+			}
+			else if (valType === "string")
+			{
+				rv = true;
+			}
+			else if (valType === "symbol")
+			{
+				rv = true;
+			}
+			else if(valType === "undefined")
+			{
+				//AKA val == null
+				rv = true;
+			}
+			else if(Array.isArray(val))
+			{
+				rv = true;
+			}
+			return rv;
+		},
+		isFunction : function (obj)
+		{    
+			"use strict";
+			if(JSMeta.isNullOrUndefined(obj))
+				return false;
+			
+			if(typeof obj !== "function")
+				return false;
+			
+			return true;
+		},
+		/* reflection utilities ---------------------------------------------------------*/
+		getMemberNamesAsArray : function(obj)
+		{    
+			"use strict";
+			if(JSMeta.isNullOrUndefined(obj))
+				throw "nullOrUndefined";
+			
+			var rv = [];
+			for(var p in obj)
+			{
+				rv.push(p);
+			}
+			return rv;
+		},
+		/* does the object have the provided properties */
+		hasMembers : function(/* object to test membership on */ obj /*, list of members */)
+		{    
+			"use strict";
+			if(JSMeta.isNullOrUndefined(obj))
+				throw "nullOrUndefined";
+			
+			var args = arguments;
+			for(var i=1;i<args.length;i++)
+			{
+				if(!obj.hasOwnProperty(args[i]))
+					return false;
+			}
+			return true;
+		},
+		/*function reflection ---------------------------------------------------*/
+		/* parses the raw function signature */
+		getFunctionRawSignature : function(func)
+		{   
+			"use strict";
+			if(!JSMeta.isFunction(func))
+				throw "not a function";
+			
+			//parse to 1st "{"
+			var origText = func.toString();
+			var sig = origText.split('{',1)[0];
+			
+			//scrub trailing whitespace
+			sig = sig.trim();
+
+			return sig;
+		},
+		/* parses the function text to get a list of argument names */
+		getFunctionArgNamesAsArray : function(func) 
+		{   
+			"use strict";
+			if(!JSMeta.isFunction(func))
+				throw "not a function";
+			
+			// First match everything inside the function argument parens.
+			var args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
+
+			// Split the arguments string into an array comma delimited.
+			return args.split(',').map(function(arg) {
+				// Ensure no inline comments are parsed and trim the whitespace.
+				return arg.replace(/\/\*.*\*\//, '').trim();
+			}).filter(function(arg) {
+				// Ensure no undefined values are added.
+				return arg;
+			})
+		},
+		hasSameFunctionSignature : function(fnA, fnB, isStrict)
+		{    
+			"use strict";
+			if(JSMeta.isNullOrUndefined(fnA))
+				throw "nullOrUndefined";
+			if(JSMeta.isNullOrUndefined(fnB))
+				throw "nullOrUndefined";
+
+			if(!JSMeta.isFunction(fnA))
+				throw "not a function";
+			if(!JSMeta.isFunction(fnB))
+				throw "not a function";
+
+
+			//run a strict mode compare
+			if(isStrict)
+			{
+				var sigA = JSMeta.getFunctionRawSignature(fnA);
+				var sigB = JSMeta.getFunctionRawSignature(fnB);
+				
+				return sigA === sigB;
+			}
+
+			//otherwise just compare argument names
+			var namesA = JSMeta.getFunctionArgNamesAsArray(fnA);
+			var namesB = JSMeta.getFunctionArgNamesAsArray(fnB);
+			
+			if(!ArrayUtil.areArraysEqual(namesA, namesB))
+				return false;
+			
+			return true;
+		},
+		/* converts an arguments array into a map using the parsed argument names as keys*/
+		convertFunctionArgsToMap : function(func, args)
+		{    
+			"use strict";
+			if(!JSMeta.isFunction(func))
+				throw "not a function";
+			
+			var rv = {};
+			
+			//if there are no args, there is no return data
+			if(JSMeta.isNullOrUndefined(args))
+				return rv;
+			
+			//parse the arg names
+			var argNames = JSMeta.getFunctionArgNamesAsArray(func);
+			
+			//if there are no names for the map, there is no return data
+			if(JSMeta.isNullOrUndefined(argNames) || argNames.length == 0)
+				return rv;
+			
+			for(var i=0; i < argNames.length; i++)
+			{
+				rv[argNames[i]] = args[i];    
+			}
+
+			return rv;
+		},
+
+
+		/* does the function have the provided args in the specified order */
+		hasFunctionArgNames : function( /*expects a function */ func, /* expects an array of names */ expectedArgNames)
+		{    
+			"use strict";
+			if(!JSMeta.isFunction(func))
+				throw "not a function";
+			
+			var argNames = JSMeta.getFunctionArgNamesAsArray(func);
+			
+			if(JSMeta.isNullOrUndefined(argNames) &&
+			   JSMeta.isNullOrUndefined(expectedArgNames))
+			{    
+				return true;
+			}
+			
+			if(!Array.isArray(expectedArgNames))
+				throw "not an array";
+			
+			var max = Math.max(expectedArgNames, argNames);
+			
+			for(var i=0; i< max; i++)
+			{
+				var v1 = expectedArgNames[i];
+				var v2 = argNames[i];
+				
+				if(v1 !== v2)
+					return false;
+			}
+			return true;
+		},
+			/* 
+			examines obj to see if a function of name exists, and if its args pass the validation function
+		*/
+		isValidFunctionSignature : function(obj, name, /* function(argNames) */ argValidationFn)
+		{    
+			"use strict";
+			if(JSMeta.isNullOrUndefined(obj))
+				throw "nullOrUndefined";
+			if(JSMeta.isNullOrUndefined(name))
+				throw "nullOrUndefined";
+			if(JSMeta.isNullOrUndefined(argValidationFn))
+				throw "nullOrUndefined";
+		   
+			//does the member exist?
+			if(!JSMeta.hasMembers(obj, name))
+				return false;
+			
+			//is it a function?
+			var fn = obj[name];
+			if(!JSMeta.isFunction(fn))
+				return false;
+			
+			//get the args
+			var argNames = JSMeta.getFunctionArgNamesAsArray(obj[name]);
+			
+			//do the args validate?
+			try
+			{    
+				if(!argValidationFn(argNames))
+					return false;
+			}
+			catch(e)
+			{
+				return false;    
+			}
+			return true;
+		},
+		/*object meta ----------------------------------------------------------*/
+		/*  has same member names && functions have same signatures.  uses templateObj as the members to query for */
+		hasSameObjectSignaturesAsTemplate : function(templateObj, testObj, /* expects array of members to ignore*/ excludeList)
+		{    
+			"use strict";
+			if(JSMeta.isNullOrUndefined(templateObj))
+				throw "nullOrUndefined";
+			if(JSMeta.isNullOrUndefined(testObj))
+				throw "nullOrUndefined";
+
+			var hasExclusions = !JSMeta.isNullOrUndefined(excludeList);
+			
+			for(var p in templateObj)
+			{
+				if(hasExclusions)
+					if(p in excludeList)
+						continue;
+				
+				if(!(p in testObj))
+					return false;
+
+				var templateMember = templateObj[p];
+				if(JSMeta.isFunction(templateMember))
+				{
+					var templateArgs = JSMeta.getFunctionArgNamesAsArray(templateMember);
+					var testMember = testObj[p];
+					if(!JSMeta.hasFunctionArgNames(testMember, templateArgs))
+						return false;
+				}
+			}
+
+			return true;
+		}
+	};
+	(function(){
+		
+		JSMeta.arrayutil = ArrayUtil;
+		
+		//lock it down
+		Object.freeze(JSMeta);   
+	})();
+
+
+	//wire up the exports
+	
+	// Node.js
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = JSMeta;
+    }
+    // AMD / RequireJS
+    else if (typeof define !== 'undefined' && define.amd) {
+        define([], function () {
+            return JSMeta;
+        });
+    }
+    // included directly via <script> tag
+    else {
+        this.jsmeta = JSMeta;
+    }
+	
+})();
+	
---
+++ test/test.js
@@ -0,0 +1,316 @@
+const Code = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+const describe = lab.describe;
+const it = lab.it;
+const before = lab.before;
+const after = lab.after;
+const expect = Code.expect;
+
+var jsmeta = require('./../jsmeta.js')();
+
+describe('jsmeta', () => {
+
+	var builder;
+
+    before((done) => {
+        done();
+    });
+
+    after((done) => {
+        done();
+    });
+
+    it('does some general tests', (done) => {
+	
+		(function(){
+			//TESTS-------------------------------------------------
+			//isnullorundefined
+			(function()
+			{
+				var u;
+
+				if(!jsmeta.isNullOrUndefined(u))
+					throw "kack!";
+
+				if(!jsmeta.isNullOrUndefined(null))
+					throw "kack!";
+
+				if(jsmeta.isNullOrUndefined("asd"))
+					throw "kack!";
+			})();
+			//isprimitive
+			(function()
+			{
+				var u;
+						
+				if(!jsmeta.isPrimitive(u))
+					throw "kack!";
+
+				if(!jsmeta.isPrimitive(null))
+					throw "kack!";
+
+				if(!jsmeta.isPrimitive(1))
+					throw "kack!";
+
+				if(!jsmeta.isPrimitive("asdfads"))
+					throw "kack!";
+
+				if(!jsmeta.isPrimitive(true))
+					throw "kack!";
+
+				if(jsmeta.isPrimitive({}))
+					throw "kack!";
+			})();
+			
+			//isFunction
+			(function()
+			{
+				var u;
+				
+				if(!jsmeta.isFunction(function(){}))
+					throw "kack!";
+
+				if(jsmeta.isFunction(null))
+					throw "kack!";
+
+				if(jsmeta.isFunction(u))
+					throw "kack!";
+			})();
+			
+			//hasMembers
+			(function()
+			{
+				//build test object
+				var testObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+
+						//do nothing
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				if(!jsmeta.hasMembers(testObj, "propA", "propB"))
+					throw "kack";
+				
+				if(!jsmeta.hasMembers(testObj, "testFn"))
+					throw "kack";
+				
+				if(jsmeta.hasMembers(testObj, "nonmember"))
+					throw "kack";
+				
+			})();
+			
+			//getFunctionRawSignature
+			(function()
+			{
+				//build test object
+				var testObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+
+						//do nothing
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				var sig = jsmeta.getFunctionRawSignature(testObj.testFn);
+				if(sig !==  'function testFn_fake(a,b,c,d)')
+					throw "kack";
+			
+			})();
+			
+			//getFunctionArgNamesAsArray
+			(function()
+			{
+				//build test object
+				var testObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+
+						//do nothing
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				var args = jsmeta.getFunctionArgNamesAsArray(testObj.testFn);
+				if(!jsmeta.arrayutil.areArraysEqual(args, ['a','b','c','d']))
+					throw "kack";
+			
+			})();
+			
+			//convertFunctionArgsToMap
+			(function()
+			{
+				//build test object
+				var testObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+
+						//do nothing
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				var args = jsmeta.convertFunctionArgsToMap(testObj.testFn, [1 , 2, 3, 4]);
+				
+				if(args.a !== 1)
+					throw "kack";
+				
+				if(args.b !== 2)
+					throw "kack";
+				
+				if(args.c !== 3)
+					throw "kack";
+				
+				if(args.d !== 4)
+					throw "kack";
+			
+			})();
+			
+			//getMemberNamesAsArray
+			(function()
+			{
+				//build test object
+				var testObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+
+						//do nothing
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				var members = jsmeta.getMemberNamesAsArray(testObj);
+				
+				if(!jsmeta.arrayutil.areArraysEqual(members, ['propA', 'propB', 'testFn']))
+					throw "kack";
+
+			
+			})();
+			
+			
+			//hasFunctionArgNames
+			(function()
+			{
+				//build test object
+				var testObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+
+						//do nothing
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				if(!jsmeta.hasFunctionArgNames(testObj.testFn, ['a', 'b', 'c', 'd']))
+					throw "kack";
+			
+			})();
+			
+			//hasSameObjectSignaturesAsTemplate
+			(function()
+			{
+				//build template object
+				var templateObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				//build test object
+				var testObj = {
+					propA : 'B',
+					propB : 'C',
+					testFn : function testFn_fake2(a,b,c,d){
+						//do nothing
+					}
+				};
+				
+				if(!jsmeta.hasSameObjectSignaturesAsTemplate(templateObj, testObj))
+					throw "kack";
+			
+			})();
+			
+			
+			//isValidFunctionSignature
+			(function()
+			{
+				//build test object
+				var testObj = {
+					propA : 'A',
+					propB : 'B',
+					testFn : function testFn_fake(a,b,c,d){
+
+						//do nothing
+						var rv = a + b +c +d;
+						return rv;
+					}
+				};
+				
+				if(!jsmeta.isValidFunctionSignature(testObj, "testFn", function(args)
+				{
+					return jsmeta.arrayutil.areArraysEqual(args, ['a', 'b', 'c', 'd']);
+				}))
+					throw "kack";
+				
+			})();
+			
+			//hasSameFunctionSignature
+			(function()
+			{
+				//build test object
+				var sig = function testFn_fake(a,b,c,d){};
+				var sameExactSig =  function testFn_fake(a,b,c,d){};
+				var sameSig = function testFn_fake2(a,b,c,d){};
+				var diffSig1 = function testFn_fake2(a,b,c){};
+				var diffSig2 = function testFn_fake2(a,b,c,d, e){};
+				var diffSig3 = function testFn_fake2(a,b,c,e){};
+				var diffSig4 = function testFn_fake2(a1,b,c,d){};
+				var diffSig5 = function testFn_fake2(){};
+				
+				if(!jsmeta.hasSameFunctionSignature(sig, sameExactSig, true))
+					throw "kack";
+				
+				if(!jsmeta.hasSameFunctionSignature(sig, sameSig, false))
+					throw "kack";
+				
+				if(!jsmeta.hasSameFunctionSignature(sig, sameSig))
+					throw "kack";
+				
+				if(jsmeta.hasSameFunctionSignature(sig, diffSig1))
+					throw "kack";
+				if(jsmeta.hasSameFunctionSignature(sig, diffSig2))
+					throw "kack";
+				if(jsmeta.hasSameFunctionSignature(sig, diffSig3))
+					throw "kack";
+				if(jsmeta.hasSameFunctionSignature(sig, diffSig4))
+					throw "kack";
+				if(jsmeta.hasSameFunctionSignature(sig, diffSig5))
+					throw "kack";		
+			})();
+
+			
+		})();
+		
+		done();
+    });
+
+});
